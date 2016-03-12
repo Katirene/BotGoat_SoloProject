0ddd0bf2d79a54b694f303b1c226608405ba514d
Katirene <kat.traxler@gmail.com>
2016-03-12 16:41:08 -0600

    changed routing

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ var app = express();
 var router = express.Router();
 var bodyParser = require('body-parser');
 var postTime = require('./routes/postTime');
-var pause = require('./routes/postTime/pause');
+var pause = require('./routes/postTime');
 
 
 app.use(bodyParser.json());
