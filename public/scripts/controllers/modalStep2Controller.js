e693c1d6d3ddd29bc7c854f16a0eaf9563c14ef6
Katirene <kat.traxler@gmail.com>
2016-03-14 12:55:39 -0500

    radio buttons hiding on selection

--- public/scripts/controllers/modalStep2Controller.js
+++ public/scripts/controllers/modalStep2Controller.js
@@ -1,15 +1,15 @@
 myApp.controller('StepTwoController', function ($scope) {
-
-    $scope.myCronOutput = '';
-
-    $scope.myConfig = {
-        options: {
-            allowMinute: true,
-            allowDay: true,
-            allowWeek : true,
-            allowMonth : true,
-            allowYear : false
-        }
-    };
-
+//
+//    $scope.myCronOutput = '';
+//
+//    $scope.myConfig = {
+//        options: {
+//            allowMinute: true,
+//            allowDay: true,
+//            allowWeek : true,
+//            allowMonth : true,
+//            allowYear : false
+//        }
+//    };
+//
 });
