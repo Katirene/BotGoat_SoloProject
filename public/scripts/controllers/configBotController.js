a853affa2e37428710f060179d8cc89637102f7c
Katirene <kat.traxler@gmail.com>
2016-03-14 12:44:16 -0500

    commiting before major DF plan

--- public/scripts/controllers/configBotController.js
+++ public/scripts/controllers/configBotController.js
@@ -3,50 +3,60 @@ myApp.controller('BotConfigController', function ($scope, $http) {
 
     console.log('In BotConfigController');
 
-    var pause = false;
+    //Moved to Home Controller
+    //var pause = false;
 
-    $scope.hourTweet = '';
-    $scope.myCronOutput = '';
+    //Moved to Step 3 Controller
+    //$scope.hourTweet = '';
 
-    $scope.myConfig = {
-        options: {
-            allowMinute: true,
-            allowDay: true,
-            allowWeek : true,
-            allowMonth : true,
-            allowYear : false
-        }
-    };
+  //Moved to step 2 controller
+    //$scope.myCronOutput = '';
+    //
+    //$scope.myConfig = {
+    //    options: {
+    //        allowMinute: true,
+    //        allowDay: true,
+    //        allowWeek : true,
+    //        allowMonth : true,
+    //        allowYear : false
+    //    }
+    //};
 
-    $scope.hourlyTweet = function() {
-        var hourTweet = {cronData: $scope.myCronOutput, hourTweet: $scope.hourTweet};
-        console.log(hourTweet);
-        postTime(hourTweet);
-    };
 
-    $scope.pause = function() {
-        pause ^= true;
+    //Moved to Step 3 controller.
+    //$scope.hourlyTweet = function() {
+    //    var hourTweet = {cronData: $scope.myCronOutput, hourTweet: $scope.hourTweet};
+    //    console.log(hourTweet);
+    //    postTime(hourTweet);
+    //};
 
-        pauseBot(pause);
-    };
+    //Moved to Home Controller.
+    //$scope.pause = function() {
+    //    pause ^= true;
+    //
+    //    pauseBot(pause);
+    //};
 
+
+    //Moved to Step 3 Controller.
     //hourTweet is the object that has 2 properties. hour: sets the interval of time, hourTweet sets the status.
-    function postTime(hourTweet) {
-        $http.post('/postTime', hourTweet).then(function(response) {
-            console.log('Tweet with time interval has been send to Server');
-            console.log(response.config.data.hourTweet);
-            $scope.latestTweet = response.config.data.hourTweet;
-            console.log($scope.latestTweet);
-        });
-    }
+    //function postTime(hourTweet) {
+    //    $http.post('/postTime', hourTweet).then(function(response) {
+    //        console.log('Tweet with time interval has been send to Server');
+    //        console.log(response.config.data.hourTweet);
+    //        $scope.latestTweet = response.config.data.hourTweet;
+    //        console.log($scope.latestTweet);
+    //    });
+    //}
 
-    function pauseBot(shouldPause) {
-        console.log('PauseBot(): pause=' + shouldPause);
-
-        $http.put('/postTime', {pause: shouldPause}).then(function() {
-            console.log('Pause info sent to server')
-        });
-    }
+    //Moved to Home Controller
+    //function pauseBot(shouldPause) {
+    //    console.log('PauseBot(): pause=' + shouldPause);
+    //
+    //    $http.put('/postTime', {pause: shouldPause}).then(function() {
+    //        console.log('Pause info sent to server')
+    //    });
+    //}
 
 
 });
---
+++ public/scripts/controllers/modalStep1Controller.js
@@ -0,0 +1,6 @@
+myApp.controller('StepOneController', function ($scope) {
+
+    $scope.colorValue= '';
+
+
+});
